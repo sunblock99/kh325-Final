17a5d3a378ed32c6057bdb1247b3a1c2754e4232
Sunblock <sunblock99@kakao.com>
2022-10-03 20:54:19 +0900

    헤더 코드 수정 - 선형채 10-03

--- kh325_Final/src/main/java/com/kh/tour/HomeController.java
+++ kh325_Final/src/main/java/com/kh/tour/HomeController.java
@@ -11,27 +11,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
 	}
--- kh325_Final/src/main/webapp/WEB-INF/views/common/darkHeader.jsp
+++ kh325_Final/src/main/webapp/WEB-INF/views/common/darkHeader.jsp
@@ -10,7 +10,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Directory Theme by Bootstrapious</title>
+    <title>TOGETHER</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
