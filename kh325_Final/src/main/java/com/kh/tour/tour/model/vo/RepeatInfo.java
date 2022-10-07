f55899d716ab0ce09beaaf93ed523f5781517633
user <user@LAPTOP-GC5CGKLN>
2022-10-07 22:29:12 +0900

    RepeatCourse , RepeatInfo vo 작성
    10-07 수아

---
+++ kh325_Final/src/main/java/com/kh/tour/tour/model/vo/RepeatInfo.java
@@ -0,0 +1,19 @@
+package com.kh.tour.tour.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RepeatInfo {
+
+	private int    reSeq     ;     //반복시퀀스
+	private int    contentId  ;    //콘텐츠ID
+	private int    serialNum  ;    //반복일련번호
+	private int    fldgubun   ;    //일련번호
+	private String infoName   ;    //제목
+	private String infoText   ;    //내용
+
+}
