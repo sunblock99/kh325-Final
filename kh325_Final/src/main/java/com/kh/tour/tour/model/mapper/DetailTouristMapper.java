cfe98627c5f56e9dde7254239c593146fca56bb6
NAIM <rlaskdla97@gmail.com>
2022-10-09 04:15:52 +0900

    detailTourist mapper (xml,java) 생성 푸시 김나임 10-09

---
+++ kh325_Final/src/main/java/com/kh/tour/tour/model/mapper/DetailTouristMapper.java
@@ -0,0 +1,11 @@
+package com.kh.tour.tour.model.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kh.tour.tour.model.vo.DetailTourist;
+
+@Mapper
+public interface DetailTouristMapper {
+
+	int insertTouristDetail (DetailTourist dTourist);
+}
