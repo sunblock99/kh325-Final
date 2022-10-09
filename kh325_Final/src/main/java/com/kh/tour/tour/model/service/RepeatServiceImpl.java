ef0e3742b2909c63ffc6633bf93b32f4b2760cca
NAIM <rlaskdla97@gmail.com>
2022-10-10 00:15:29 +0900

    RepeatServiceIpml 수정 푸시 김나임 10-10

--- kh325_Final/src/main/java/com/kh/tour/tour/model/service/RepeatServiceImpl.java
+++ kh325_Final/src/main/java/com/kh/tour/tour/model/service/RepeatServiceImpl.java
@@ -1,6 +1,7 @@
 package com.kh.tour.tour.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kh.tour.tour.model.mapper.RepeatMapper;
@@ -8,6 +9,7 @@ import com.kh.tour.tour.model.vo.RepeatCourse;
 import com.kh.tour.tour.model.vo.RepeatHotel;
 import com.kh.tour.tour.model.vo.RepeatInfo;
 
+@Service
 public class RepeatServiceImpl implements RepeatService{
 	
 	@Autowired
