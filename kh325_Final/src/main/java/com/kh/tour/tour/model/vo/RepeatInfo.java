cde0061e42133532d2ec39a51c3eba5a39089875
Sunblock <sunblock99@kakao.com>
2022-10-09 11:25:08 +0900

    RepeatInfo vo 수정 선형채
    10-09

--- kh325_Final/src/main/java/com/kh/tour/tour/model/vo/RepeatInfo.java
+++ kh325_Final/src/main/java/com/kh/tour/tour/model/vo/RepeatInfo.java
@@ -11,6 +11,7 @@ public class RepeatInfo {
 
 	private int    reSeq     ;     //반복시퀀스
 	private int    contentId  ;    //콘텐츠ID
+	private int contentTypeId;     //콘텐츠타입ID
 	private int    serialNum  ;    //반복일련번호
 	private int    fldgubun   ;    //일련번호
 	private String infoName   ;    //제목
