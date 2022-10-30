22c3798975e73d7a1cdf5c3659efb9dbb049c018
jungsuah <sua971109@naver.com>
2022-10-31 01:15:25 +0900

    Tourcontroller/ eventSearch.jsp 수정 푸시
    10-31 수아

--- kh325_Final/src/main/java/com/kh/tour/tour/controller/TourController.java
+++ kh325_Final/src/main/java/com/kh/tour/tour/controller/TourController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.tour.common.util.PageInfo;
 import com.kh.tour.course.model.service.CourseService;
@@ -92,7 +93,8 @@ public class TourController {
 			@RequestParam(value = "tourType", required = false) String tourType, // 관광지타입 1개
 			@RequestParam(value = "areaCode", required = false) List<String> areaCode, // 체크된 지역코드 리스트
 			@RequestParam(value = "cat1", required = false) String cat1, // 대분류 1개
-			@RequestParam(value = "cat2", required = false) List<String> cat2 // 중분류 리스트
+			@RequestParam(value = "cat2", required = false) List<String> cat2, // 중분류 리스트
+			@SessionAttribute(name = "loginMember", required = false) Member loginMember
 	) {
 		log.info("param : " + param.toString());
 		System.out.println("가지고 들어온 파람값: " + param.toString());
@@ -132,11 +134,13 @@ public class TourController {
 			System.out.println("찾아온 cat2List : " + cat2List.toString());
 			model.addAttribute("cat2List", cat2List);
 		}
+		
 
 		PageInfo pageInfo = new PageInfo(page, 10, tService.getTourCount(param, tourType, areaCode, cat1, cat2), 9);
 		int tourListCount = tService.getTourCount(param, tourType, areaCode, cat1, cat2);
 		List<Tour> tourlist = tService.getTourList(pageInfo, param, tourType, areaCode, cat1, cat2);
 		System.out.println("찾아온 관광지 리스트 : " + tourlist);
+		
 
 		model.addAttribute("areaCode", areaCode);
 		model.addAttribute("cat1", cat1);
@@ -232,13 +236,20 @@ public class TourController {
 
 	) {
 		System.out.println("review 작성 컨트롤러 호출");
-		String gogo = this.tourDetailInfo(model, contentId, contentTypeId);
-
+		
 		log.info("리뷰 작성 요청");
 		DetailReview detailReview = new DetailReview(0, contentId, userNo, star, content, null, null);
 		int result = tService.insertReview(detailReview);
-
-		return gogo;
+//		Tour tour = tService.findByContentId(contentId);
+		
+		if(result > 0) {
+			model.addAttribute("msg", "리플이 등록되었습니다.");
+		}else {
+			model.addAttribute("msg", "리플 작성에 실패했습니다.");
+		}
+		model.addAttribute("location", request.getHeader("Referer"));
+//		model.addAttribute("location", "/tourDetailInfo.do?contentId=" + detailReview.getContentId() + "&contentTypeId=" + tour.getContentTypeId());
+		return "/common/msg";
 	}
 
 	@GetMapping("/tourLike.do") // 찜하기 기능
@@ -258,6 +269,7 @@ public class TourController {
 			System.out.println("contentId null");
 		}
 
+		Tour tour = tService.findByContentId(contentId);
 		int userNo = loginMember.getUserNo(); // 로그인한 회원의 userNo 받아오기
 		System.out.println("현재 로그인된 회원번호는 :" + userNo);
 		List<TourLike> tourLike = tService.selectTourLikeList(userNo); // 회원번호로 찜리스트 조회해오기
@@ -267,13 +279,11 @@ public class TourController {
 			int result2 = tService.insertTourLike(userNo, contentId);
 			if (result2 > 0) {
 				model.addAttribute("msg", "찜목록에 넣기 성공하였습니다.");
-				model.addAttribute("location", "/tourSearch.do");
 			} else {
 				model.addAttribute("msg", "찜목록에 넣기 실패하였습니다.");
-				model.addAttribute("location", "/tourSearch.do");
 			}
 		}
-
+	
 		if (tourLike.isEmpty() == false) {
 			for (int i = 0; i < tourLike.size(); i++) {
 				if (tourLike.get(i).getContentId() == contentId) { // 찜목록에 내가 찜하고 싶은 관광지가 이미 있는 경우
@@ -284,71 +294,23 @@ public class TourController {
 					int result = tService.insertTourLike(userNo, contentId);
 					if (result > 0) {
 						model.addAttribute("msg", "찜목록에 넣기 성공하였습니다.");
-						model.addAttribute("location", "/tourSearch.do");
 						break;
 					} else {
 						model.addAttribute("msg", "찜목록에 넣기 실패하였습니다.");
-						model.addAttribute("location", "/tourSearch.do");
 						break;
 					}
 				}
 			}
 		}
-		return "/common/msg";
-	}
-
-	@GetMapping("/eventLike.do") // 행사찜하기 기능
-	public String eventLike(Model model, HttpServletRequest request,
-			@SessionAttribute(name = "loginMember", required = false) Member loginMember,
-			@RequestParam("contentId") int contentId) {
-
-		if (loginMember == null || loginMember.getUserNo() < 0) {
-			model.addAttribute("msg", "로그인이 필요합니다.");
-			model.addAttribute("location", "/loginView");
-			return "/common/msg";
-		}
-
-		if (contentId > 0) {
-			System.out.println("가지고 들어온 contentId는 :" + contentId);
+		if (request.getHeader("Referer").contains("contentTypeId")) {
+			model.addAttribute("location", "/tourDetailInfo.do?contentId=" + tour.getContentId()
+					+ "&contentTypeId=" + tour.getContentTypeId());
 		} else {
-			System.out.println("contentId null");
-		}
-
-		int userNo = loginMember.getUserNo(); // 로그인한 회원의 userNo 받아오기
-		System.out.println("현재 로그인된 회원번호는 :" + userNo);
-		List<TourLike> tourLike = tService.selectTourLikeList(userNo); // 회원번호로 찜리스트 조회해오기
-		System.out.println("회원번호로 조회한 찜목록 :" + tourLike.toString());
-
-		if (tourLike.isEmpty() == true) {
-			int result2 = tService.insertTourLike(userNo, contentId);
-			if (result2 > 0) {
-				model.addAttribute("msg", "찜목록에 넣기 성공하였습니다.");
-				model.addAttribute("location", "/eventSearch.do");
-			} else {
-				model.addAttribute("msg", "찜목록에 넣기 실패하였습니다.");
-				model.addAttribute("location", "/eventSearch.do");
-			}
-		} else if (tourLike.isEmpty() == false) {
-			for (int i = 0; i < tourLike.size(); i++) {
-				if (tourLike.get(i).getContentId() == contentId) { // 찜목록에 내가 찜하고 싶은 관광지가 이미 있는 경우
-					model.addAttribute("msg", "이미 있는 찜목록 입니다.");
-					model.addAttribute("location", "/eventSearch.do");
-					break;
-				} else {// 찜목록에 내가 찜하고 싶은 관광지가 없는 경우 -> insert
-					int result = tService.insertTourLike(userNo, contentId);
-					if (result > 0) {
-						model.addAttribute("msg", "찜목록에 넣기 성공하였습니다.");
-						model.addAttribute("location", "/eventSearch.do");
-						break;
-					} else {
-						model.addAttribute("msg", "찜목록에 넣기 실패하였습니다.");
-						model.addAttribute("location", "/eventSearch.do");
-						break;
-					}
-				}
-			}
+			model.addAttribute("location", request.getHeader("Referer"));
 		}
 		return "/common/msg";
 	}
+	
+
 
 }
